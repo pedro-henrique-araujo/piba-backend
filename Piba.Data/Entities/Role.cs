6c6a4a8be3cf61bd88b404e574c1573f08d87e1a
Pedro <60669964+pedro-henrique-araujo@users.noreply.github.com>
2024-08-03 22:29:36 -0300

    Add Identity Framework Tables

---
+++ Piba.Data/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Piba.Data.Entities
+{
+    public class Role : IdentityRole<Guid>
+    {
+    }
+}
---
+++ Piba.Data/Entities/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Piba.Data.Entities
+{
+    public class UserLogin : IdentityUserLogin<Guid>
+    {
+    }
+}
